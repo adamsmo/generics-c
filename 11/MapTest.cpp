af26df31695919be94b7ed9139bbe41f2329ad54
Adam Smolarek <adam@Mac-mini-Adam.local>
2016-05-23 16:49:25 +0200

    add test

---
+++ 11/MapTest.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adam Smolarek on 19.05.2016.
+//
+
+#include "MapTest.h"
---
+++ 11/MapTest.h
@@ -0,0 +1,14 @@
+//
+// Created by Adam Smolarek on 19.05.2016.
+//
+
+#ifndef ARM_I2C_MAPTEST_H
+#define ARM_I2C_MAPTEST_H
+
+
+class MapTest {
+
+};
+
+
+#endif //ARM_I2C_MAPTEST_H
